6ca870951570578739e88022cbf2b72c5f7d5996
Colin Alston <colin.alston@gmail.com>
2015-02-02 16:22:53 +0200

    Use correct environment variable

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,4 +1,4 @@
-manage="${VENV}/bin/python ${INSTALLDIR}/${REPO}/manage.py"
+manage="${VENV}/bin/python ${INSTALLDIR}/${NAME}/manage.py"
 
 $manage syncdb --noinput --no-initial-data --migrate
 $manage collectstatic --noinput
