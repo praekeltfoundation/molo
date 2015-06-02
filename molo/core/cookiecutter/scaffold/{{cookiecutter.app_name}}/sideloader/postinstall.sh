cc966d1b9d71c6d6a0fd4ad74e96ed8ddc17ab35
Milton Madanda <madandat@gmail.com>
2015-06-02 18:33:49 +0200

    ensure we use the production settings

--- molo/core/cookiecutter/scaffold/{{cookiecutter.app_name}}/sideloader/postinstall.sh
+++ molo/core/cookiecutter/scaffold/{{cookiecutter.app_name}}/sideloader/postinstall.sh
@@ -1,6 +1,6 @@
 cd "${INSTALLDIR}/${NAME}/{{cookiecutter.app_name}}/"
 manage="${VENV}/bin/python ${INSTALLDIR}/${NAME}/{{cookiecutter.app_name}}/manage.py"
 
-$manage migrate
-$manage compress
-$manage collectstatic --noinput
+$manage migrate --settings={{cookiecutter.app_name}}.settings.production
+$manage compress --settings={{cookiecutter.app_name}}.settings.production
+$manage collectstatic --noinput --settings={{cookiecutter.app_name}}.settings.production
