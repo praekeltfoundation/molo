9dbb2872380981e41b358126942aca7cd032b11d
Milton Madanda <madandat@gmail.com>
2015-06-03 08:11:29 +0200

    add basic models

---
+++ molo/core/migrations/0003_articlepage_languagepage_main_sectionpage.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('wagtailimages', '0005_make_filter_spec_unique'),
+        ('wagtailcore', '0010_change_page_owner_to_null_on_delete'),
+        ('core', '0002_create_homepage'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='ArticlePage',
+            fields=[
+                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
+                ('subtitle', models.TextField()),
+                ('image', models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.SET_NULL, blank=True, to='wagtailimages.Image', null=True)),
+            ],
+            options={
+                'verbose_name': 'Article',
+            },
+            bases=('wagtailcore.page',),
+        ),
+        migrations.CreateModel(
+            name='LanguagePage',
+            fields=[
+                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
+                ('code', models.CharField(max_length=255)),
+            ],
+            options={
+                'verbose_name': 'Language',
+            },
+            bases=('wagtailcore.page',),
+        ),
+        migrations.CreateModel(
+            name='Main',
+            fields=[
+                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('wagtailcore.page',),
+        ),
+        migrations.CreateModel(
+            name='SectionPage',
+            fields=[
+                ('page_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='wagtailcore.Page')),
+                ('description', models.TextField()),
+            ],
+            options={
+                'verbose_name': 'Section',
+            },
+            bases=('wagtailcore.page',),
+        ),
+    ]
--- molo/core/models.py
+++ molo/core/models.py
@@ -1,7 +1,72 @@
-# from django.db import models
+from django.db import models
 
 from wagtail.wagtailcore.models import Page
+from wagtail.wagtailsearch import index
+from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 
 class HomePage(Page):
     pass
+
+
+class Main(Page):
+    parent_page_types = []
+    subpage_types = ['core.LanguagePage']
+
+
+class LanguagePage(Page):
+    code = models.CharField(max_length=255)
+
+    parent_page_types = ['core.Main']
+    subpage_types = ['core.HomePage', 'core.SectionPage']
+
+    class Meta:
+        verbose_name = 'Language'
+
+LanguagePage.content_panels = [
+    FieldPanel('title'),
+    FieldPanel('code'),
+]
+
+
+class SectionPage(Page):
+    description = models.TextField(null=True, blank=True)
+
+    subpage_types = ['core.ArticlePage']
+    search_fields = Page.search_fields + (
+        index.SearchField('description'),
+    )
+
+    class Meta:
+        verbose_name = 'Section'
+
+SectionPage.content_panels = [
+    FieldPanel('title'),
+    FieldPanel('description'),
+]
+
+
+class ArticlePage(Page):
+    subtitle = models.TextField(null=True, blank=True)
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+'
+    )
+
+    subpage_types = []
+    search_fields = Page.search_fields + (
+        index.SearchField('subtitle'),
+    )
+
+    class Meta:
+        verbose_name = 'Article'
+
+ArticlePage.content_panels = [
+    FieldPanel('title'),
+    FieldPanel('subtitle'),
+    ImageChooserPanel('image'),
+]
