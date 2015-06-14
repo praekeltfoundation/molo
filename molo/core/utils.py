db0fc1746b13a920072fdea65a0b3b0f4d9742e0
Milton Madanda <madandat@gmail.com>
2015-06-14 10:16:14 +0200

    change param default for get locale code

--- molo/core/utils.py
+++ molo/core/utils.py
@@ -2,7 +2,7 @@ from django.utils.translation import to_locale
 from django.conf import settings
 
 
-def get_locale_code(language_code=settings.LANGUAGE_CODE):
+def get_locale_code(language_code=None):
     # Simple implementation that uses the ISO 639-1 (2 char codes)
     # discarding the country code
     language_code = language_code or settings.LANGUAGE_CODE
