737b17fd1558faa39f254a9aa8ecd84b27ff260e
Milton Madanda <madandat@gmail.com>
2015-06-23 15:48:07 +0200

    fix tests

--- run-tests.sh
+++ run-tests.sh
@@ -6,6 +6,6 @@ flake8 molo
 molo scaffold testapp
 pip install -e testapp
 coverage erase
-coverage run `which py.test` --ds=test_settings --verbose molo "$@"
+coverage run `which py.test` --ds=testapp.settings --verbose molo "$@"
 coverage report
 rm -rf testapp
