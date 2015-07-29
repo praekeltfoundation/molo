57432fdf0e5ccbb3fe3e11133ed0a004fed719ec
Simon de Haan <simon@praekeltfoundation.org>
2015-07-29 19:22:53 +0200

    do not run coverage for pypy

--- run-tests.sh
+++ run-tests.sh
@@ -6,7 +6,11 @@ flake8 molo
 molo scaffold testapp
 flake8 testapp
 pip install -e testapp
-coverage erase
-coverage run `which py.test` --ds=testapp.settings --verbose molo "$@"
-coverage report
+if [ ${TRAVIS_PYTHON_VERSION} == "pypy" ]; then
+    coverage erase
+    coverage run `which py.test` --ds=testapp.settings --verbose molo "$@"
+    coverage report
+else
+    `which py.test` --ds=testapp.settings --verbose molo "$@"
+fi
 rm -rf testapp
