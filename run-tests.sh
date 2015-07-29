25f018a923b26a6956236ff03503f9594e2b2e18
Simon de Haan <simon@praekeltfoundation.org>
2015-07-29 19:59:32 +0200

    doh, wrong order

--- run-tests.sh
+++ run-tests.sh
@@ -7,10 +7,10 @@ molo scaffold testapp
 flake8 testapp
 pip install -e testapp
 if [ ${TRAVIS_PYTHON_VERSION} == "pypy" ]; then
+    `which py.test` --ds=testapp.settings --verbose molo "$@"
+else
     coverage erase
     coverage run `which py.test` --ds=testapp.settings --verbose molo "$@"
     coverage report
-else
-    `which py.test` --ds=testapp.settings --verbose molo "$@"
 fi
 rm -rf testapp
