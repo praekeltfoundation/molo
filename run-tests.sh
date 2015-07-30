a8d3e699321b1b22b8cfb3478600c9efdc44522e
Simon de Haan <simon@praekeltfoundation.org>
2015-07-30 09:07:10 +0200

    remove coverage stuff from run-tests

--- run-tests.sh
+++ run-tests.sh
@@ -6,11 +6,5 @@ flake8 molo
 molo scaffold testapp
 flake8 testapp
 pip install -e testapp
-if [ ${TRAVIS_PYTHON_VERSION:-""} == "pypy" ]; then
-    `which py.test` --ds=testapp.settings --verbose molo "$@"
-else
-    coverage erase
-    coverage run `which py.test` --ds=testapp.settings --verbose molo "$@"
-    coverage report
-fi
+`which py.test` --ds=testapp.settings --verbose molo "$@"
 rm -rf testapp
