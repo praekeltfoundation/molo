71095ba03873fe750b54d96ec3e201fd749e83cb
Simon de Haan <simon@praekeltfoundation.org>
2015-07-30 08:58:17 +0200

    more travis fiddling

--- run-tests.sh
+++ run-tests.sh
@@ -6,7 +6,7 @@ flake8 molo
 molo scaffold testapp
 flake8 testapp
 pip install -e testapp
-if [ ${TRAVIS_PYTHON_VERSION} == "pypy" ]; then
+if [ ${TRAVIS_PYTHON_VERSION:-""} == "pypy" ]; then
     `which py.test` --ds=testapp.settings --verbose molo "$@"
 else
     coverage erase
