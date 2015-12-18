d31bea24240bb784891f04a525e383ab842f0647
Milton Madanda <madandat@gmail.com>
2015-12-18 20:06:43 +0200

    clean up views tests

---
+++ molo/core/tests/base.py
@@ -0,0 +1,96 @@
+import json
+
+from django.contrib.contenttypes.models import ContentType
+from django.utils.text import slugify
+
+from wagtail.wagtailcore.models import Site, Page
+
+from molo.core.models import LanguagePage, Main, SectionPage, ArticlePage
+
+
+class MoloTestCaseMixin(object):
+
+    def mk_main(self):
+        # Create page content type
+        page_content_type, created = ContentType.objects.get_or_create(
+            model='page',
+            app_label='wagtailcore'
+        )
+
+        # Create root page
+        Page.objects.create(
+            title="Root",
+            slug='root',
+            content_type=page_content_type,
+            path='0001',
+            depth=1,
+            numchild=1,
+            url_path='/',
+        )
+
+        main_content_type, created = ContentType.objects.get_or_create(
+            model='main', app_label='core')
+
+        # Create a new homepage
+        self.main = Main.objects.create(
+            title="Main",
+            slug='main',
+            content_type=main_content_type,
+            path='00010001',
+            depth=2,
+            numchild=0,
+            url_path='/home/',
+        )
+        self.main.save_revision().publish()
+
+        self.english = LanguagePage(
+            title='English',
+            code='en',
+            slug='english')
+        self.main.add_child(instance=self.english)
+        self.english.save_revision().publish()
+
+        # Create a site with the new homepage set as the root
+        Site.objects.all().delete()
+        self.site = Site.objects.create(
+            hostname='localhost', root_page=self.main, is_default_site=True)
+
+    def mk_sections(self, parent, count=2, **kwargs):
+        sections = []
+
+        for i in range(count):
+            data = {}
+            data.update({
+                'title': 'Test Section %s' % (i, ),
+            })
+            data.update(kwargs)
+            data.update({
+                'slug': slugify(data['title'])
+            })
+            section = SectionPage(**data)
+            parent.add_child(instance=section)
+            section.save_revision().publish()
+            sections.append(section)
+        return sections
+
+    def mk_pages(self, parent, count=2, **kwargs):
+        pages = []
+
+        for i in range(count):
+            data = {}
+            data.update({
+                'title': 'Test page %s' % (i, ),
+                'subtitle': 'Sample page description for %s' % (i, ),
+                'body': json.dumps([{
+                    'type': 'paragraph',
+                    'value': 'Sample page content for %s' % (i, )}]),
+            })
+            data.update(kwargs)
+            data.update({
+                'slug': slugify(data['title'])
+            })
+            page = ArticlePage(**data)
+            parent.add_child(instance=page)
+            page.save_revision().publish()
+            pages.append(page)
+        return pages
