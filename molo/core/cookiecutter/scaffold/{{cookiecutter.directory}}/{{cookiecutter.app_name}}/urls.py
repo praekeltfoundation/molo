53ac2ee37ef478a9fd7b135b8b35c7ea804258b3
Milton Madanda <madandat@gmail.com>
2016-01-19 08:57:14 +0200

    remove unneeded views for smart css

--- molo/core/cookiecutter/scaffold/{{cookiecutter.directory}}/{{cookiecutter.app_name}}/urls.py
+++ molo/core/cookiecutter/scaffold/{{cookiecutter.directory}}/{{cookiecutter.app_name}}/urls.py
@@ -9,7 +9,6 @@ from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtailsearch import urls as wagtailsearch_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
 from wagtail.wagtailcore import urls as wagtail_urls
-from .views import SmartCSS
 
 # implement CAS URLs in a production setting
 if settings.ENABLE_SSO:
@@ -24,7 +23,6 @@ else:
 
 urlpatterns += patterns(
     '',
-    url(r'^smart-css/$', SmartCSS.as_view(), name='smart-css'),
     url(r'^django-admin/', include(admin.site.urls)),
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^search/', include(wagtailsearch_urls)),
--- molo/core/cookiecutter/scaffold/{{cookiecutter.directory}}/{{cookiecutter.app_name}}/views.py
+++
@@ -1,6 +0,0 @@
-from django.views.generic.base import TemplateView
-
-
-class SmartCSS(TemplateView):
-
-    template_name = "smart-css.html"
