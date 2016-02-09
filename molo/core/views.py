66a0f34fe55db1eb232243631f8666d44babe9e9
Milton Madanda <madandat@gmail.com>
2016-02-09 10:40:50 +0200

    add translation views for translation actions

--- molo/core/views.py
+++ molo/core/views.py
@@ -1,6 +1,14 @@
+from django.contrib import messages
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
 from django.utils.translation import LANGUAGE_SESSION_KEY
+from django.utils.translation import ugettext as _
+
+from molo.core.utils import generate_slug
+from molo.core.models import PageTranslation
+
+from wagtail.wagtailcore.models import Page
 
 
 def locale_set(request, locale):
@@ -10,3 +18,39 @@ def locale_set(request, locale):
 
 def health(request):
     return HttpResponse(status=200)
+
+
+def add_translation(request, page_id, locale):
+    def get_translated_page_for(page, locale):
+        translated_page = None
+        for t in page.translations.all():
+            if (hasattr(t.translated_page.specific, 'language') and
+                    t.translated_page.specific.language == locale):
+                translated_page = t.translated_page.specific
+                break
+        return translated_page
+
+    _page = get_object_or_404(Page, id=page_id)
+    page = _page.specific
+
+    if not hasattr(page, 'translations'):
+        messages.add_message(
+            request, messages.INFO, _('That page is not translatable.'))
+        return redirect(reverse('wagtailadmin_home'))
+
+    # redirect to edit page if translation already exists for this locale
+    translated_page = get_translated_page_for(page, locale)
+    if translated_page:
+        return redirect(
+            reverse('wagtailadmin_pages:edit', args=[translated_page.id]))
+
+    # create translation and redirect to edit page
+    new_slug = generate_slug(page.title)
+    translation = page.__class__(
+        title=page.title, slug=new_slug, language=locale)
+    page.get_parent().add_child(instance=translation)
+    translation.save_revision()
+    PageTranslation.objects.create(page=page, translated_page=translation)
+
+    return redirect(
+        reverse('wagtailadmin_pages:edit', args=[translation.id]))
--- molo/core/wagtail_hooks.py
+++ molo/core/wagtail_hooks.py
@@ -1,5 +1,8 @@
+from django.conf.urls import url
+
 from wagtailmodeladmin.options import(
     ModelAdmin, wagtailmodeladmin_register)
+from wagtail.wagtailcore import hooks
 
 from molo.core.models import LanguagePage
 
@@ -16,3 +19,13 @@ class LanguageModelAdmin(ModelAdmin):
     menu_order = 100
 
 wagtailmodeladmin_register(LanguageModelAdmin)
+
+
+@hooks.register('register_admin_urls')
+def urlconf_translations():
+    return [
+        url(
+            r'^translations/add/(?P<page_id>\d+)/(?P<locale>\w+)/$',
+            'molo.core.views.add_translation',
+            name='add_translation'),
+    ]
