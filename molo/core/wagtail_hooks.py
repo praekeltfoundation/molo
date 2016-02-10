f7c0795665c10ea237bd0c033f28f89240d2f752
Milton Madanda <madandat@gmail.com>
2016-02-10 18:40:25 +0200

    filter pages by main language - not neat, but does the job

--- molo/core/wagtail_hooks.py
+++ molo/core/wagtail_hooks.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
+from django.db.models import Q
 
 from wagtailmodeladmin.options import(
     ModelAdmin, wagtailmodeladmin_register)
 from wagtail.wagtailcore import hooks
+from wagtail.wagtailcore.models import Page
 
 from molo.core.models import SiteLanguage
 
@@ -29,3 +31,21 @@ def urlconf_translations():
             'molo.core.views.add_translation',
             name='add_translation'),
     ]
+
+
+@hooks.register('construct_explorer_page_queryset')
+def show_main_language_only(parent_page, pages, request):
+    main_language = SiteLanguage.objects.filter(is_main_language=True).first()
+
+    if main_language and not parent_page.depth == 1:
+        translatable_pages = Page.objects.filter(
+            Q(articlepage__isnull=False) |
+            Q(sectionpage__isnull=False) |
+            Q(homepage__isnull=False))
+        main_pages = [
+            p.id
+            for p in translatable_pages
+            if p.specific.language.id == main_language.id]
+        return pages.filter(id__in=main_pages)
+
+    return pages
