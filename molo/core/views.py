54c5953d4e9c8c4833e88add85e7c77bf0f69155
Saeed Marzban <Saeed.mb8@gmail.com>
2016-03-13 21:33:10 +0200

    added known_plugins.py

--- molo/core/views.py
+++ molo/core/views.py
@@ -1,8 +1,11 @@
+import pkg_resources
+import requests
+
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.utils.translation import LANGUAGE_SESSION_KEY
-import pkg_resources
-import requests
+
+from molo.core.known_plugins import known_plugins
 
 
 def locale_set(request, locale):
@@ -16,38 +19,34 @@ def health(request):
 
 def versions(request):
     comparison_url = "https://github.com/praekelt/%s/compare/%s...%s"
-    packages = [('molo.core', 'Molo'), ('molo.profiles', 'Profiles'),
-                ('molo.commenting', 'Commenting'), ('molo.polls', 'Polls'),
-                ('molo.yourwords', 'YourWords'), ('molo.servicedirectory',
-                'Srvice Directory')]
-    packages_info = []
-    for package in packages:
+    plugins_info = []
+    for plugin in known_plugins():
         try:
-            package_version = (
-                pkg_resources.get_distribution(package[0])).version
+            plugin_version = (
+                pkg_resources.get_distribution(plugin[0])).version
 
-            pypi_version = get_pypi_version(package[0])
+            pypi_version = get_pypi_version(plugin[0])
 
-            if package[0] == 'molo.core':
+            if plugin[0] == 'molo.core':
                 compare_versions_link = comparison_url % (
-                    'molo', package_version, pypi_version)
+                    'molo', plugin_version, pypi_version)
             else:
                 compare_versions_link = comparison_url % (
-                    package[0], package_version, pypi_version)
+                    plugin[0], plugin_version, pypi_version)
 
-            packages_info.append((package[1], pypi_version,
-                                  package_version, compare_versions_link))
+            plugins_info.append((plugin[1], pypi_version,
+                                 plugin_version, compare_versions_link))
         except:
-            pypi_version = get_pypi_version(package[0])
-            packages_info.append((package[1], pypi_version, "-",
+            pypi_version = get_pypi_version(plugin[0])
+            plugins_info.append((plugin[1], pypi_version, "-",
                                  ""))
 
     return render(request, 'admin/versions.html', {
-        'packages_info': packages_info,
+        'plugins_info': plugins_info,
     })
 
 
-def get_pypi_version(package_name):
+def get_pypi_version(plugin_name):
     url = "https://pypi.python.org/pypi/%s/json"
-    content = requests.get(url % package_name).json()
+    content = requests.get(url % plugin_name).json()
     return content.get('info').get('version')
