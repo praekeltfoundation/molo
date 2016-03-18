fb3c73a922e386bb11628253c3d0df7755f6b792
justinvdm <justinvdm.0.1.0@gmail.com>
2016-03-18 11:24:56 +0200

    Fix false positive updateSites() action test

--- client/tests/utils.js
+++ client/tests/utils.js
@@ -1,6 +1,12 @@
 import constant from 'lodash/constant';
+import identity from 'lodash/identity';
 
 
 export function resolvesTo(v) {
   return constant(Promise.resolve(v));
+}
+
+
+export function thunk(fn) {
+  return fn(identity);
 }
--- client/tests/views/git-importer/actions.test.js
+++ client/tests/views/git-importer/actions.test.js
@@ -2,11 +2,11 @@ import { expect } from 'chai';
 import * as actions from 'src/views/git-importer/actions';
 import fixtures from 'tests/views/git-importer/fixtures';
 import { conj } from 'src/utils';
-import { resolvesTo } from 'tests/utils';
+import { thunk, resolvesTo } from 'tests/utils';
 
 
 describe(`actions`, () => {
-  describe(`updateSites`, done => {
+  describe(`updateSites`, () => {
     it(`should return the list of sites to update with`, () => {
       let api = conj(fixtures('api'), {
         sites: resolvesTo([{
@@ -18,20 +18,19 @@ describe(`actions`, () => {
         }])
       });
 
-      actions.updateSites(api)(action => {
-        expect(action).to.deep.equal({
-          type: 'update-sites',
-          sites: [{
-            id: 'foo-id',
-            name: 'foo'
-          }, {
-            id: 'bar-id',
-            name: 'bar'
-          }]
-        });
-
-        done();
-      });
+      return thunk(actions.updateSites(api))
+        .then(action => expect(action).to.deep.equal({
+          type: 'UPDATE_SITES',
+          payload: {
+            sites: [{
+              id: 'foo-id',
+              name: 'foo'
+            }, {
+              id: 'bar-id',
+              name: 'bar'
+            }]
+          }
+        }));
     });
   });
 });
