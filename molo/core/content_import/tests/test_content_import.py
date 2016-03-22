a2364c8ec0356f9ec5e51aeb427e4018e030049b
Saeed Marzban <Saeed.mb8@gmail.com>
2016-03-22 10:10:51 +0200

    added tests

--- molo/core/content_import/tests/test_content_import.py
+++ molo/core/content_import/tests/test_content_import.py
@@ -108,10 +108,23 @@ class ContentImportTestCase(
             self.workspace, count=1, locale='spa_ES',
             primary_category=None)
 
+        # page with linked page
+        [page_en] = self.create_pages(
+            self.workspace, count=1, locale='eng_GB',
+            primary_category=cat_eng_1.uuid)
+        [page_with_linked_page] = self.create_pages(
+            self.workspace, count=1, locale='eng_GB',
+            linked_pages=[page_en.uuid], primary_category=cat_eng_1.uuid)
+
         for i in range(0, 10):
             self.workspace.save(es_pages1[i].update({
                 'source': en_pages1[i].uuid,
             }), 'Added source to page.')
+
+            self.workspace.save(en_pages1[i].update({
+                'author_tags': ['love'],
+            }), 'Added author_tags to page.')
+
             self.workspace.save(es_pages2[i].update({
                 'source': en_pages2[i].uuid,
             }), 'Added source to page.')
@@ -125,7 +138,7 @@ class ContentImportTestCase(
             self.workspace.S(eg_models.Category).all().count(), 6)
 
         self.assertEquals(
-            self.workspace.S(eg_models.Page).all().count(), 46)
+            self.workspace.S(eg_models.Page).all().count(), 48)
         self.assertEquals(
             self.workspace.S(eg_models.Localisation).all().count(), 2)
 
@@ -137,7 +150,14 @@ class ContentImportTestCase(
             {'locale': 'spa_ES', 'site_language': 'es', 'is_main': False}])
 
         self.assertEquals(SectionPage.objects.all().count(), 4)
-        self.assertEquals(ArticlePage.objects.all().count(), 44)
+        self.assertEquals(ArticlePage.objects.all().count(), 46)
+        self.assertEquals(
+            ArticlePage.objects.filter(metadata_tags__name='love').count(), 10)
+
+        page = ArticlePage.objects.get(uuid=page_with_linked_page.uuid)
+        linked_page = ArticlePage.objects.get(uuid=page_en.uuid)
+        self.assertEquals(page.body.stream_data[2],
+                          {u'type': u'page', u'value': linked_page.pk})
 
         # run import twice
         ContentImportHelper(self.workspace).import_content_for([
@@ -145,5 +165,5 @@ class ContentImportTestCase(
             {'locale': 'spa_ES', 'site_language': 'es', 'is_main': False}])
 
         self.assertEquals(SectionPage.objects.all().count(), 4)
-        self.assertEquals(ArticlePage.objects.all().count(), 44)
+        self.assertEquals(ArticlePage.objects.all().count(), 46)
         self.assertEquals(FooterPage.objects.all().count(), 4)
