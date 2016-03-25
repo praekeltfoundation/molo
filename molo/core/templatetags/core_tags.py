97eff0971b6f3c20a57c152cd4815aa817b4fb9c
Milton Madanda <madandat@gmail.com>
2016-03-25 10:05:25 +0200

    use template tag to load translations for articles and sections

--- molo/core/templatetags/core_tags.py
+++ molo/core/templatetags/core_tags.py
@@ -11,7 +11,7 @@ register = template.Library()
 )
 def section_listing_homepage(context):
     request = context['request']
-    locale_code = context['locale_code']
+    locale_code = context.get('locale_code')
 
     if request.site:
         sections = request.site.root_page.specific.sections()
@@ -32,7 +32,7 @@ def section_listing_homepage(context):
 )
 def latest_listing_homepage(context, num_count=5):
     request = context['request']
-    locale_code = context['locale_code']
+    locale_code = context.get('locale_code')
 
     if request.site:
         articles = request.site.root_page.specific\
@@ -51,7 +51,7 @@ def latest_listing_homepage(context, num_count=5):
 @register.inclusion_tag('core/tags/bannerpages.html', takes_context=True)
 def bannerpages(context):
     request = context['request']
-    locale_code = context['locale_code']
+    locale_code = context.get('locale_code')
 
     if request.site:
         pages = request.site.root_page.specific.bannerpages()
@@ -69,7 +69,7 @@ def bannerpages(context):
 @register.inclusion_tag('core/tags/footerpage.html', takes_context=True)
 def footer_page(context):
     request = context['request']
-    locale_code = context['locale_code']
+    locale_code = context.get('locale_code')
 
     if request.site:
         pages = request.site.root_page.specific.footers()
@@ -86,14 +86,25 @@ def footer_page(context):
 @register.inclusion_tag('core/tags/breadcrumbs.html', takes_context=True)
 def breadcrumbs(context):
     self = context.get('self')
+    locale_code = context.get('locale_code')
+
     if self is None or self.depth <= 2:
         # When on the home page, displaying breadcrumbs is irrelevant.
         ancestors = ()
     else:
-        ancestors = Page.objects.ancestor_of(
-            self, inclusive=True).filter(depth__gt=2)
+        ancestors = Page.objects.live().ancestor_of(
+            self, inclusive=True).filter(depth__gt=2).specific()
+
+    translated_ancestors = []
+    for p in ancestors:
+        if hasattr(p, 'get_translation_for'):
+            translated_ancestors.append(
+                p.get_translation_for(locale_code, True) or p)
+        else:
+            translated_ancestors.append(p)
+
     return {
-        'ancestors': ancestors,
+        'ancestors': translated_ancestors,
         'request': context['request'],
     }
 
@@ -126,21 +137,47 @@ def translation(page, locale):
 
 
 @register.assignment_tag(takes_context=True)
-def load_section_articles(
+def load_descendant_articles_for_section(
         context, section, featured_in_homepage=None, featured_in_section=None,
         featured_in_latest=None, count=5):
+    '''
+    Returns all descendant articles (filtered using the parameters)
+    If the `locale_code` in the context is not the main language, it will
+    return the translations of the live articles.
+    '''
     page = section.get_main_language_page()
+    locale = context.get('locale_code')
 
-    locale = context['locale_code']
-    qs = ArticlePage.objects.live().order_by(
-        '-first_published_at').descendant_of(page)
+    qs = ArticlePage.objects.live().descendant_of(page).filter(
+        languages__language__is_main_language=True)
 
     if featured_in_homepage is not None:
         qs = qs.filter(featured_in_homepage=featured_in_homepage)
+
     if featured_in_latest is not None:
         qs = qs.filter(featured_in_latest=featured_in_latest)
+
     if featured_in_section is not None:
         qs = qs.filter(featured_in_section=featured_in_section)
+
+    if not locale:
+        return qs[:count]
+
+    return [a.get_translation_for(locale) or a for a in qs[:count]]
+
+
+@register.assignment_tag(takes_context=True)
+def load_child_articles_for_section(context, section, count=5):
+    '''
+    Returns all child articles
+    If the `locale_code` in the context is not the main language, it will
+    return the translations of the live articles.
+    '''
+    page = section.get_main_language_page()
+    locale = context.get('locale_code')
+
+    qs = ArticlePage.objects.live().child_of(page).filter(
+        languages__language__is_main_language=True)
 
     if not locale:
         return qs[:count]
