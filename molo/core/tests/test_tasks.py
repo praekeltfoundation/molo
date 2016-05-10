d63b37638eb8bd3d9d7cc16f989eed6be7120084
Saeed Marzban <Saeed.mb8@gmail.com>
2016-05-10 19:36:29 +0200

    added tests for the second language

--- molo/core/tests/test_tasks.py
+++ molo/core/tests/test_tasks.py
@@ -4,11 +4,15 @@ import pytest
 from django.test import TestCase
 
 
-from molo.core.models import SiteSettings
+from molo.core.models import LanguagePage
 from molo.core.tests.base import MoloTestCaseMixin
+
 from testapp.tasks import rotate_content
+
 from wagtail.wagtailimages.models import Image
 from wagtail.wagtailimages.tests.utils import get_test_image_file
+from wagtail.wagtailcore.models import Site
+from wagtail.contrib.settings.context_processors import SettingsProxy
 
 
 @pytest.mark.django_db
@@ -22,6 +26,7 @@ class TestTasks(TestCase, MoloTestCaseMixin):
         )
 
         self.mk_main()
+
         self.yourmind = self.mk_section(
             self.english, title='Your mind')
         self.yourmind_sub = self.mk_section(
@@ -34,13 +39,64 @@ class TestTasks(TestCase, MoloTestCaseMixin):
         first_article_old = self.english.latest_articles()[0].pk
         second_last_article_old = self.english.latest_articles()[8].pk
         last_article_old = self.english.latest_articles()[9].pk
-        SiteSettings.content_rotation = True
+
+        site = Site.objects.get(is_default_site=True)
+        settings = SettingsProxy(site)
+        site_settings = settings['core']['SiteSettings']
+
+        site_settings.content_rotation = True
         d = datetime.now()
-        SiteSettings.content_rotation_time = d.hour
+        site_settings.content_rotation_time = d.hour
+        site_settings.save()
         rotate_content()
 
         self.assertEquals(self.english.latest_articles().count(), 10)
-        self.assertNotEquals(first_article_old, self.english.latest_articles()[0].pk)
-        self.assertEquals(first_article_old, self.english.latest_articles()[1].pk)
-        self.assertNotEquals(last_article_old, self.english.latest_articles()[9].pk)
-        self.assertEquals(second_last_article_old, self.english.latest_articles()[9].pk)
+        self.assertNotEquals(
+            first_article_old, self.english.latest_articles()[0].pk)
+        self.assertEquals(
+            first_article_old, self.english.latest_articles()[1].pk)
+        self.assertNotEquals(
+            last_article_old, self.english.latest_articles()[9].pk)
+        self.assertEquals(
+            second_last_article_old, self.english.latest_articles()[9].pk)
+
+        self.french = LanguagePage(
+            title='French',
+            code='fr',
+            slug='french')
+        self.main.add_child(instance=self.french)
+        self.french.save_revision().publish()
+
+        self.yourmind_fr = self.mk_section(
+            self.french, title='Your mind french')
+        self.yourmind_sub_fr = self.mk_section(
+            self.yourmind_fr, title='Your mind subsection french')
+
+        self.mk_articles(
+            self.yourmind_sub_fr, count=10, featured_in_latest=True)
+        self.mk_articles(
+            self.yourmind_sub_fr, count=10, featured_in_latest=False)
+        self.assertEquals(self.french.latest_articles().count(), 10)
+        first_article_old = self.french.latest_articles()[0].pk
+        second_last_article_old = self.french.latest_articles()[8].pk
+        last_article_old = self.french.latest_articles()[9].pk
+
+        site = Site.objects.get(is_default_site=True)
+        settings = SettingsProxy(site)
+        site_settings = settings['core']['SiteSettings']
+
+        site_settings.content_rotation = True
+        d = datetime.now()
+        site_settings.content_rotation_time = d.hour
+        site_settings.save()
+        rotate_content()
+
+        self.assertEquals(self.french.latest_articles().count(), 10)
+        self.assertNotEquals(
+            first_article_old, self.french.latest_articles()[0].pk)
+        self.assertEquals(
+            first_article_old, self.french.latest_articles()[1].pk)
+        self.assertNotEquals(
+            last_article_old, self.french.latest_articles()[9].pk)
+        self.assertEquals(
+            second_last_article_old, self.french.latest_articles()[9].pk)
