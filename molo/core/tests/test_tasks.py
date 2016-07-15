0bb70622321caf1b9c20671acf607b023204d401
Codie Roelf <codie@praekelt.com>
2016-07-15 16:49:08 +0200

    adding tests for featured in homepage rotation

--- molo/core/tests/test_tasks.py
+++ molo/core/tests/test_tasks.py
@@ -7,7 +7,7 @@ from django.test import TestCase
 from molo.core.models import SiteLanguage, FooterPage
 from molo.core.tests.base import MoloTestCaseMixin
 
-from molo.core.tasks import rotate_content
+from molo.core.tasks import rotate_content, rotate_featured_in_latest_content
 
 from wagtail.wagtailcore.models import Site
 from wagtail.contrib.settings.context_processors import SettingsProxy
@@ -64,3 +64,50 @@ class TestTasks(TestCase, MoloTestCaseMixin):
             last_article_old, self.main.latest_articles()[9].pk)
         self.assertEquals(
             second_last_article_old, self.main.latest_articles()[9].pk)
+
+    def test_homepage_rotation(self):
+
+        def get_featured_articles(section):
+            return section.featured_in_homepage_articles()
+
+        site = Site.objects.get(is_default_site=True)
+        settings = SettingsProxy(site)
+        site_settings = settings['core']['SiteSettings']
+
+        site_settings.featured_in_latest_rotation = True
+        d = datetime.now()
+        site_settings.content_rotation_time = d.hour
+        site_settings.save()
+
+        non_rotating_articles = self.mk_articles(
+            self.yourmind, count=3, featured_in_homepage=False)
+        rotate_featured_in_latest_content()
+        for article in non_rotating_articles:
+            self.assertFalse(article.featured_in_latest)
+        self.assertEquals(get_featured_articles(self.yourmind).count(), 0)
+        self.yourmind_sub.featured_in_latest_rotation = True
+        self.yourmind.featured_in_latest_rotation = False
+        self.yourmind.save_revision().publish()
+        self.yourmind_sub.save_revision().publish()
+        self.mk_articles(
+            self.yourmind_sub, count=10, featured_in_homepage=True)
+        self.mk_articles(
+            self.yourmind_sub, count=10, featured_in_homepage=False)
+        self.assertEquals(
+            get_featured_articles(self.yourmind_sub).count(), 10)
+        first_article_old = get_featured_articles(self.yourmind_sub)[0].pk
+        second_last_article_old = get_featured_articles(self.yourmind_sub)[8].pk
+        last_article_old = get_featured_articles(self.yourmind_sub)[9].pk
+
+        rotate_featured_in_latest_content()
+        self.assertEquals(
+            get_featured_articles(self.yourmind_sub).count(), 10)
+        self.assertNotEquals(
+            first_article_old, get_featured_articles(self.yourmind_sub)[0].pk)
+        self.assertEquals(
+            first_article_old, get_featured_articles(self.yourmind_sub)[1].pk)
+        self.assertNotEquals(
+            last_article_old, get_featured_articles(self.yourmind_sub)[9].pk)
+        self.assertEquals(
+            second_last_article_old,
+            get_featured_articles(self.yourmind_sub)[9].pk)
