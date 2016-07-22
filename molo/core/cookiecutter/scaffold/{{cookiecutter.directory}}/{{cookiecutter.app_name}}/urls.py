4256f9f131ed89fd4b968d43ea1485c1ca8495df
Codie Roelf <codie@praekelt.com>
2016-07-22 12:12:40 +0200

    removing analytics urls from cookiecutter, adding to molo urls

--- molo/core/cookiecutter/scaffold/{{cookiecutter.directory}}/{{cookiecutter.app_name}}/urls.py
+++ molo/core/cookiecutter/scaffold/{{cookiecutter.directory}}/{{cookiecutter.app_name}}/urls.py
@@ -28,7 +28,6 @@ urlpatterns += patterns(
     url(r'^admin/', include(wagtailadmin_urls)),
     url(r'^documents/', include(wagtaildocs_urls)),
     url(r'search/$', search, name='search'),
-    url(r'^djga/', include('google_analytics.urls')),
 
 {% for app_name, regex in cookiecutter.include %}
     url(r'{{regex}}',
