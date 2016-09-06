2c3bb39f60490de4ff063ffef4eef423266541b8
Milton Madanda <madandat@gmail.com>
2016-09-06 14:31:49 +0200

    add middleware to ignore accept language header

--- molo/core/middleware.py
+++ molo/core/middleware.py
@@ -49,6 +49,23 @@ class Custom403Middleware(object):
         return response
 
 
+class ForceDefaultLanguageMiddleware(object):
+    """
+    Ignore Accept-Language HTTP headers
+
+    This will force the I18N machinery to always choose settings.LANGUAGE_CODE
+    as the default initial language, unless another one is set via
+    sessions or cookies
+
+    Should be installed *before* any middleware that checks
+    request.META['HTTP_ACCEPT_LANGUAGE'],
+    namely django.middleware.locale.LocaleMiddleware
+    """
+    def process_request(self, request):
+        if 'HTTP_ACCEPT_LANGUAGE' in request.META:
+            del request.META['HTTP_ACCEPT_LANGUAGE']
+
+
 class AdminLocaleMiddleware(object):
     """Ensures that the admin locale doesn't change with user selection"""
     def process_request(self, request):
