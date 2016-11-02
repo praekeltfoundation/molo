c233ea9610c7e563b7ae09b9596ab6171a48b8dd
Phala Ramonyai <phalamag@gmail.com>
2016-11-02 20:11:29 +0200

    Fix migration file's name

--- molo/core/migrations/0046_sitesettings_enable_clickable_tags.py
+++ molo/core/migrations/0046_sitesettings_enable_clickable_tags.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('core', '0044_add_topic_of_the_day_fields'),
+        ('core', '0045_bannerpage_external_link'),
     ]
 
     operations = [
