fbbee24a71f840131748bc8ca1cadc7759c58d52
Phala Ramonyai <phalamag@gmail.com>
2016-11-20 19:57:26 +0200

    Fix name of Aritlce Parent Chooser view as used by the URLs

--- molo/core/content_import/api/urls.py
+++ molo/core/content_import/api/urls.py
@@ -5,5 +5,5 @@ from molo.core.content_import.api import admin_views
 
 urlpatterns = [
     url(r"^import-articles/$", admin_views.ArticleImportView.as_view(), name="article-import"),
-    url(r"^parent/$", admin_views.ChooseParentView.as_view(model_admin=admin_views.ArticleModelAdmin()), name="test-parent"),
+    url(r"^parent/$", admin_views.ArticleChooserView.as_view(model_admin=admin_views.ArticleModelAdmin()), name="test-parent"),
 ]
