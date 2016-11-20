0e1188cb8d50274d7938b2d7cd6cd544feb766a6
Phala Ramonyai <phalamag@gmail.com>
2016-11-20 19:01:23 +0200

    Override Parent chooser

--- molo/core/content_import/api/admin_views.py
+++ molo/core/content_import/api/admin_views.py
@@ -1,39 +1,63 @@
 """
 Views for importing content from another wagtail instance
 """
-from django.views.generic import FormView
+from django.views.generic import FormView, View
 from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import render
 
-from wagtailmodeladmin.views import CreateView
+from wagtailmodeladmin.views import CreateView, ChooseParentView
 
 from molo.core.content_import.api import forms
 from molo.core.content_import.api import importers
 
 from molo.core.models import ArticlePage
 from wagtailmodeladmin.options import ModelAdmin as WagtailModelAdmin
+from wagtailmodeladmin.helpers import PagePermissionHelper
 
 
-class ImportView(FormView):
+class ArticleImportView(FormView):
     """
     Test view to see if the importing of a single article can work
     """
-    form_class = forms.MainImportForm
-    success_url = reverse_lazy("molo_api:test-api-import-view")
-    template_name = "core/api/import.html"
+    form_class = forms.ArticleImportForm
+    success_url = reverse_lazy("molo_api:article-import")
+    template_name = "core/api/article_import.html"
 
-    # def get_context_data(self, **kwargs):
-    #     pass
+    importer = importers.ArticlePageImporter()
 
     def get_form_kwargs(self):
         # pass valid importer to the form
-        kwargs = super(ImportView, self).get_form_kwargs()
-        kwargs["importer"] = importers.ArticlePageImporter()
+        kwargs = super(ArticleImportView, self).get_form_kwargs()
+        kwargs["importer"] = self.importer
         return kwargs
 
     def form_valid(self, form):
-        data = form.save()
-        if data:
+        self.importer = form.save()
+        if self.importer.articles():
             context = self.get_context_data()
-            context["data"] = data
+            context["form"] = forms.ArticleImportForm(importer=self.importer)
             return self.render_to_response(context=context)
-        return super(ImportView, self).form_valid()
+        return super(ArticleImportView, self).form_valid()
+
+
+class ModelAdminObject(object):
+    def __init__(self):
+        self.model = ArticlePage
+        self.is_pagemodel = True
+        self.permission_helper = PagePermissionHelper(ArticlePage)
+
+
+class ArticleModelAdmin(WagtailModelAdmin):
+    model = ArticlePage
+
+
+class ChooserView(ChooseParentView):
+
+    def post(self, request, *args, **kwargs):
+        import pdb;pdb.set_trace()
+        # form = self.get_form(request)
+        # import pdb;pdb.set_trace()
+        # if form.is_valid():
+        #     parent = form.cleaned_data['parent_page']
+        # context = {'view': self, 'form': form}
+        # return render(request, self.get_template(), context)
