a674b6d03687e941e8ab518aa004227bfd70c305
Phala Ramonyai <phalamag@gmail.com>
2016-11-27 19:06:59 +0200

    Added 'file' field to images API endpoint

--- molo/core/content_import/api/endpoints.py
+++ molo/core/content_import/api/endpoints.py
@@ -1,11 +1,7 @@
 from molo.core.content_import.api.compat import ImagesAPIEndpoint
-from molo.core.content_import.api.serializers import MoloImageSerializer
 
 
 class MoloImagesAPIEndpoint(ImagesAPIEndpoint):
-    base_serializer_class = MoloImageSerializer
-
     extra_body_fields = ImagesAPIEndpoint.extra_body_fields + [
-        'file',
-        'original',
+        "file",
     ]
