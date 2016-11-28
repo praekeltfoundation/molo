f8b3554f74901077b6633c862e843183a33eb541
Codie Roelf <codie@praekelt.com>
2016-11-28 14:21:44 +0200

    adding data migration

--- molo/core/templatetags/core_tags.py
+++ molo/core/templatetags/core_tags.py
@@ -215,14 +215,14 @@ def load_descendant_articles_for_section(
 
     if featured_in_homepage is not None:
         qs = qs.filter(featured_in_homepage=featured_in_homepage).order_by(
-            '-featured_in_homepage_timestamp')
+            '-featured_in_homepage_start_date')
 
     if featured_in_latest is not None:
         qs = qs.filter(featured_in_latest=featured_in_latest)
 
     if featured_in_section is not None:
         qs = qs.filter(featured_in_section=featured_in_section).order_by(
-            '-featured_in_section_timestamp')
+            '-featured_in_section_start_date')
 
     if not locale:
         return qs[:count]
