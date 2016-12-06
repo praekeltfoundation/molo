8a6fcf9d0cf8ef53ced26f1b93b969bfc5645726
Phala Ramonyai <phalamag@gmail.com>
2016-12-06 02:16:57 +0200

    Modified API fields for ArticlePages

--- molo/core/models.py
+++ molo/core/models.py
@@ -761,7 +761,7 @@ class ArticlePage(CommentedPageMixin, TranslatablePageMixin, Page):
         "social_media_description", "social_media_image", "related_sections",
         "featured_in_latest", "featured_in_latest_start_date",
         "featured_in_latest_end_date", "featured_in_section",
-        "featured_in_section_start_date", "featured_in_section_start_date",
+        "featured_in_section_start_date", "featured_in_section_end_date",
         "featured_in_homepage", "featured_in_homepage_start_date",
         "featured_in_homepage_end_date", "feature_as_topic_of_the_day",
         "promote_date", "demote_date", "metadata_tags",
