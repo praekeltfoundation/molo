0a6078f5d0537cea9f36894b736fa274c3fa3e47
nathanbegbie <nathanbegbie@gmail.com>
2017-01-24 13:50:07 +0200

    Fix upload form to only accept .zip files

--- molo/core/cookiecutter/scaffold/{{cookiecutter.directory}}/{{cookiecutter.app_name}}/forms.py
+++ molo/core/cookiecutter/scaffold/{{cookiecutter.directory}}/{{cookiecutter.app_name}}/forms.py
@@ -4,20 +4,11 @@ from django import forms
 class MediaForm(forms.Form):
     zip_file = forms.FileField(label="Zipped Media File")
 
-    def clean_data_file(self):
-        file = self.cleaned_data['data_file']
+    def clean_zip_file(self):
+        file = self.cleaned_data['zip_file']
 
         if file:
             extension = file.name.split('.')[-1]
             if extension != 'zip':
                 raise forms.ValidationError('File Type Is Not .zip')
         return file
-    '''
-
-    def process_data(self):
-        print("I am processing the form")
-        file = self.cleaned_data['data_file'].file
-
-        # delete root media file
-        # unzip file in root folder (make sure it's called 'media')
-    '''
