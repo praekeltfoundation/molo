6d403b92eb744354f93fb190f1203faa8f94893f
TrevorOctober <trevor@praekeltconsulting.com>
2017-02-09 13:47:27 +0200

    Remove i18n_patterns

--- molo/core/cookiecutter/scaffold/{{cookiecutter.directory}}/{{cookiecutter.app_name}}/urls.py
+++ molo/core/cookiecutter/scaffold/{{cookiecutter.directory}}/{{cookiecutter.app_name}}/urls.py
@@ -2,7 +2,6 @@ import os
 
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
-from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 from django.contrib import admin
 from django.views.generic.base import TemplateView
