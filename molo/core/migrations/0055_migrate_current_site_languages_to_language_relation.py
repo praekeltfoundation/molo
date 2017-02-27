fa8b5b8b1738375e98410d5c691ba37680891764
Codie Roelf <codie@praekelt.com>
2017-02-27 16:04:04 +0200

    add data migration for current languages

---
+++ molo/core/migrations/0055_migrate_current_site_languages_to_language_relation.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+def convert_languages_to_site_language_relation(apps, schema_editor):
+    from molo.core.models import SiteLanguage, SiteLanguageRelation Main
+    main = Main.objects.all().first()
+    if main:
+        site = main.get_site()
+        if site:
+            language_setting = Languages.objects.create(
+                site_id=site.pk)
+            for language in SiteLanguage.objects.all():
+                SiteLanguageRelation.objects.create(
+                    language_setting=language_setting,
+                    locale=language.locale,
+                    is_active=language.is_active)
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0054_languages_sitelanguagerelation'),
+    ]
+
+    operations = [
+        migrations.RunPython(convert_languages_to_site_language_relation),
+    ]
