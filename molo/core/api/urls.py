dfafc60e1f83354590939eef926a9f092f256577
nathanbegbie <nathanbegbie@gmail.com>
2017-07-19 10:16:50 +0200

    Remove view for manual checking of image imports

--- molo/core/api/urls.py
+++ molo/core/api/urls.py
@@ -27,11 +27,6 @@ urlpatterns = [
         name="site-import"
     ),
     url(
-        r"^import-images/$",
-        admin_views.ImageImportView.as_view(),
-        name='image-import'
-    ),
-    url(
         r"^import-content/$",
         admin_views.MainImportView.as_view(),
         name="main-import"
