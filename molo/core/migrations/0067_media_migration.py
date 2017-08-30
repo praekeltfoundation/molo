8e887f8994b842d101522a3dc328b32a01bde5c0
Codie Roelf <codie@praekelt.com>
2017-08-30 15:16:49 +0200

    upgrade migrations

--- molo/core/migrations/0067_media_migration.py
+++ molo/core/migrations/0067_media_migration.py
@@ -2,9 +2,22 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+from django.core.management.sql import emit_post_migrate_signal
 
 
 def convert_media_to_molo_media(apps, schema_editor):
+    db_alias = schema_editor.connection.alias
+    try:
+        # Django 1.9
+        emit_post_migrate_signal(2, False, db_alias)
+    except TypeError:
+        # Django < 1.9
+        try:
+            # Django 1.8
+            emit_post_migrate_signal(2, False, 'default', db_alias)
+        except TypeError:  # Django < 1.8
+            emit_post_migrate_signal([], 2, False, 'default', db_alias)
+
     from molo.core.models import MoloMedia, ArticlePage
     from wagtailmedia.models import Media
 
@@ -25,6 +38,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('core', '0066_add_custom_media_model'),
+        ('wagtailmedia', '0003_copy_media_permissions_to_collections'),
     ]
 
     operations = [
