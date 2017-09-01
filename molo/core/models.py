349130659dac0234085d47ea150b24630f41174e
nathanbegbie <nathanbegbie@gmail.com>
2017-09-01 10:24:58 +0200

    Log exceptions and errors when creating ImportablePage

--- molo/core/models.py
+++ molo/core/models.py
@@ -302,15 +302,15 @@ class ImportableMixin(object):
             try:
                 function(*_args)
             except Exception as e:
-                raise
-                logger.log(
-                    ERROR,
-                    "Failed to create page content",
-                    {
-                        "foreign_page_id": foreign_id,
-                        "exception": e,
-                        "function": function.__name__,
-                    })
+                if logger:
+                    logger.log(
+                        ERROR,
+                        "Failed to create page content",
+                        {
+                            "foreign_page_id": foreign_id,
+                            "exception": e,
+                            "function": function.__name__,
+                        })
 
         # Handle content in nested_fields
         body = add_stream_fields(nested_fields, page)
@@ -333,14 +333,15 @@ class ImportableMixin(object):
                 try:
                     function(nested_fields, foreign_id)
                 except Exception as e:
-                    logger.log(
-                        ERROR,
-                        "Failed to record content",
-                        {
-                            "foreign_page_id": foreign_id,
-                            "exception": e,
-                            "function": function.__name__,
-                        })
+                    if logger:
+                        logger.log(
+                            ERROR,
+                            "Failed to record content",
+                            {
+                                "foreign_page_id": foreign_id,
+                                "exception": e,
+                                "function": function.__name__,
+                            })
 
         return page
 
