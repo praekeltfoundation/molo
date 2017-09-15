0643b4cb1cb2fff9102aa2d32281abf70bd7ce07
Milton Madanda <madandat@gmail.com>
2017-09-15 19:03:58 +0200

    dont cache selected language

--- molo/core/context_processors.py
+++ molo/core/context_processors.py
@@ -7,8 +7,6 @@ from molo.core.models import Languages
 def locale(request):
     locale_code = get_locale_code(get_language_from_request(request))
     cache_key_langs = "ctx_proc_languages_for_{}".format(request.site)
-    cache_key_active_lang = "ctx_proc_languages_for_{}_active_for_{}".format(
-        request.site, locale_code)
 
     languages = cache.get(cache_key_langs)
     if not languages:
@@ -16,10 +14,7 @@ def locale(request):
             is_active=True)
         cache.set(cache_key_langs, languages, 300)
 
-    selected_language = cache.get(cache_key_active_lang)
-    if not selected_language:
-        selected_language = languages.filter(locale=locale_code).first()
-        cache.set(cache_key_active_lang, selected_language, 300)
+    selected_language = languages.filter(locale=locale_code).first()
 
     return {
         'locale_code': locale_code,
