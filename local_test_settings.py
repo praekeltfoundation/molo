4f85e9afc99a433bba4d942bdb06ff32694339fb
Codie Roelf <codie@praekelt.com>
2017-11-15 15:04:05 +0200

    put back cas server settings

--- local_test_settings.py
+++ local_test_settings.py
@@ -1,6 +1,27 @@
 from testapp.settings.base import *
 
 
+ENABLE_SSO = True
+
+MIDDLEWARE_CLASSES += (
+    'molo.core.middleware.MoloCASMiddleware',
+    'molo.core.middleware.Custom403Middleware',
+)
+
+
+AUTHENTICATION_BACKENDS = (
+    'molo.profiles.backends.MoloProfilesModelBackend',
+    'molo.core.backends.MoloModelBackend',
+    'django.contrib.auth.backends.ModelBackend',
+    'molo.core.backends.MoloCASBackend',
+)
+
+CAS_SERVER_URL = 'http://testcasserver'
+CAS_ADMIN_PREFIX = '/admin/'
+LOGIN_URL = '/accounts/login/'
+CAS_VERSION = '3'
+
+
 UNICORE_DISTRIBUTE_API = 'http://testserver:6543'
 CELERY_ALWAYS_EAGER = True
 
