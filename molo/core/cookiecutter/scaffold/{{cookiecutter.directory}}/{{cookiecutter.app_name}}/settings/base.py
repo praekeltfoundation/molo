7056e3f6e96686b5dd6f38cd6e4eba3b25bdaa98
Alex Muller <alex@praekelt.org>
2017-12-19 16:32:35 +0000

    Make new molo sites default to UTC
    Now that we're storing the timezone in the database we should use
    the more standard UTC timezone for new apps.

--- molo/core/cookiecutter/scaffold/{{cookiecutter.directory}}/{{cookiecutter.app_name}}/settings/base.py
+++ molo/core/cookiecutter/scaffold/{{cookiecutter.directory}}/{{cookiecutter.app_name}}/settings/base.py
@@ -213,7 +213,7 @@ CELERYBEAT_SCHEDULE = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 LANGUAGE_CODE = environ.get('LANGUAGE_CODE', 'en')
-TIME_ZONE = 'Africa/Johannesburg'
+TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
