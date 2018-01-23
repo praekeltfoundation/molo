b6e633c2d4b712fb7a0f8459ed2f4ecefe0afe4b
Codie Roelf <codie@praekelt.com>
2018-01-23 16:32:44 +0000

    add correct logic

--- molo/core/molo_wagtail_models.py
+++ molo/core/molo_wagtail_models.py
@@ -8,6 +8,6 @@ class MoloPage(Page):
     def get_url_parts(self, request=None):
         for (site_id, root_path, root_url) in \
                 self._get_site_root_paths(request):
-            if hasattr(request, 'site') and site_id != request.site.pk:
+            if hasattr(request, 'site') and site_id == request.site.pk:
                 return super(MoloPage, self).get_url_parts(request)
         return super(MoloPage, self).get_url_parts(request)
