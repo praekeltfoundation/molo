2bb2d74dbad7d963c18b52adc8ab6e6826712bbd
Codie Roelf <codie@praekelt.com>
2018-04-23 14:55:03 +0200

    add feedback

--- molo/core/management/commands/switch_main_language.py
+++ molo/core/management/commands/switch_main_language.py
@@ -14,20 +14,33 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         new_language_locale = options.get('new_language_locale', None)
         main = Main.objects.first()
+        old_main_langauge = SiteLanguage.objects.get(is_main_language=True)
+        if main:
+            # add the new language if necessary
+            desired_main_langauge = SiteLanguage.objects.filter(
+                locale=new_language_locale)
+            if not desired_main_langauge.exists():
+                new_language = SiteLanguageRelation.objects.create(
+                    language_setting=Languages.for_site(main.get_site()),
+                    locale=new_language_locale,
+                    is_active=True)
+                self.stdout.write(self.style.NOTICE(
+                    'New Language %s created' % new_language.title))
 
-        # add the new language if necessary
-        if not SiteLanguage.objects.filter(
-                locale=new_language_locale).exists():
-            new_language = SiteLanguageRelation.objects.create(
-                language_setting=Languages.for_site(main.get_site()),
-                locale=new_language_locale,
-                is_active=True)
+            self.stdout.write(self.style.NOTICE(
+                'Langauge %s already exists' %
+                desired_main_langauge.first().title))
 
-        # switch the language in the language relation to the new language
-        for relation in LanguageRelation.objects.filter(
-                language__is_main_language=True):
-            relation.language = new_language
-            relation.save()
+            # switch the language in the language relation to the new language
+            for relation in LanguageRelation.objects.filter(
+                    language__is_main_language=True):
+                relation.language = new_language
+                relation.save()
 
-        new_language.is_main_language = True
-        new_language.save()
+            new_language.is_main_language = True
+            new_language.save()
+            self.stdout.write(self.style.NOTICE(
+                'Main language swopped from %s to %s' %
+                (old_main_langauge.title, new_language.title)))
+        else:
+            self.stdout.write(self.style.NOTICE('Main page does not exist'))
