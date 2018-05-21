603907c0a0348354f14cb926f337b3e3fb577999
Mitso <mqalaba@gmail.com>
2018-05-21 12:29:20 +0200

    update object attribute recommended_article__pk

--- molo/core/templatetags/core_tags.py
+++ molo/core/templatetags/core_tags.py
@@ -703,7 +703,7 @@ def get_recommended_articles(context, article):
     # http://stackoverflow.com/questions/4916851/django-get-a-queryset-from-array-of-ids-in-specific-order/37648265#37648265 # noqa
     # the following allows us to order the results of the querystring
     pk_list = recommended_articles.values_list(
-        'recommended_article__pk', flat=True)
+        'recommended_article', flat=True)
 
     if not pk_list:
         return []
