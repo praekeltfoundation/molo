3c51b6b4522f3630f502f4cb3b25153732cfd2bc
Codie Roelf <codie@praekelt.com>
2018-09-10 11:54:53 +0200

    fix test

--- molo/core/management/commands/add_language_to_pages.py
+++ molo/core/management/commands/add_language_to_pages.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, unicode_literals
 
 from django.core.management.base import BaseCommand
 from molo.core.models import LanguageRelation
+from molo.core.models import Page
 
 
 class Command(BaseCommand):
@@ -9,8 +10,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for relation in LanguageRelation.objects.all():
             if relation.page and relation.language:
-                relation.page.language = relation.languages
-                relation.page.save()
+                page = Page.objects.get(pk=relation.page.pk).specific
+                page.language = relation.language
+                page.save()
             else:
                 self.stdout.write(self.style.NOTICE(
                     'Relation with pk "%s" is missing either page/language'
--- molo/core/tests/test_commands.py
+++ molo/core/tests/test_commands.py
@@ -9,7 +9,7 @@ from django.utils.six import StringIO
 from molo.core.tests.base import MoloTestCaseMixin
 from molo.core.models import (
     Main, Languages, ArticlePage, Tag, ArticlePageTags, SiteLanguageRelation,
-    SiteLanguage, LanguageRelation)
+    SiteLanguage, LanguageRelation, SectionPage)
 
 
 class ManagementCommandsTest(TestCase, MoloTestCaseMixin):
@@ -46,12 +46,14 @@ class ManagementCommandsTest(TestCase, MoloTestCaseMixin):
             self.assertEqual(relation.language.locale, 'id')
 
     def test_add_language_to_pages(self):
-        self.assertNone(self.article.langauge)
-        self.assertNone(self.yourmind.langauge)
+        self.assertIsNone(self.article.language)
+        self.assertIsNone(self.yourmind.language)
         call_command('add_language_to_pages')
         english = SiteLanguage.objects.first()
-        self.assertEquals(self.article.language, english.pk)
-        self.assertEquals(self.yourmind.language, english.pk)
+        article = ArticlePage.objects.get(pk=self.article.pk)
+        yourmind = SectionPage.objects.get(pk=self.yourmind.pk)
+        self.assertEquals(article.language, english)
+        self.assertEquals(yourmind.language, english)
 
     def test_add_feature_in_latest_date_to_article(self):
         self.assertEquals(self.article.featured_in_latest_start_date, None)
