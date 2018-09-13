6000ffc7c43636421f741c029ad420fec3519b18
Codie Roelf <codie@praekelt.com>
2018-09-13 09:57:32 +0200

    flake8

--- molo/core/tests/test_commands.py
+++ molo/core/tests/test_commands.py
@@ -5,7 +5,6 @@ import tempfile
 from datetime import datetime
 from django.core.management import call_command
 from django.test import TestCase
-from django.core.urlresolvers import reverse
 from django.utils.six import StringIO
 from molo.core.tests.base import MoloTestCaseMixin
 from molo.core.models import (
