6985eec0fb8e05ee0a4bc654a09db84a8197b0ee
Codie Roelf <codie@praekelt.com>
2018-10-10 12:24:26 +0200

    catch specific exception

--- molo/core/models.py
+++ molo/core/models.py
@@ -636,7 +636,7 @@ class TranslatablePageMixinNotRoutable(object):
         try:
             return self.translated_pages.get(
                 language__is_main_language=True).specific
-        except:
+        except ObjectDoesNotExist:
             return self.specific
 
     def get_site(self):
