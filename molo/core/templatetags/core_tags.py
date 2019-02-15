79212ddd3a85baaf8e96dfe3aeae62b2443d2451
Codie Roelf <codiebeulaine@gmail.com>
2019-02-15 16:36:42 +0200

    flake8

--- molo/core/templatetags/core_tags.py
+++ molo/core/templatetags/core_tags.py
@@ -671,17 +671,17 @@ def social_media_article(context, page=None):
         twitter = site_settings.twitter_image
     else:
         twitter = False
-    
+
     if site_settings.whatsapp_sharing:
         whatsapp = site_settings.whatsapp_image
     else:
         whatsapp = False
-    
+
     if site_settings.youtube_sharing:
         youtube = site_settings.youtube_image
     else:
         youtube = False
-    
+
     if site_settings.telegram_sharing:
         telegram = site_settings.telegram_image
     else:
