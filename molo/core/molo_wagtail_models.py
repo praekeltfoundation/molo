dd34f0cb936d23cf79d61d3831466ceba3f2ea98
Codie Roelf <codiebeulaine@gmail.com>
2019-04-10 17:52:53 +0200

    flake8

--- molo/core/molo_wagtail_models.py
+++ molo/core/molo_wagtail_models.py
@@ -1,9 +1,6 @@
 from wagtail.core.models import Page
-from wagtail.core.utils import WAGTAIL_APPEND_SLASH
-from django.urls import reverse
 
 
 class MoloPage(Page):
     class Meta:
         proxy = True
-
