af9f363f3338ccb4456a7b4b8ce9b744820e525f
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2019-04-17 15:44:02 +0200

    profiles deprication warnings

--- molo/profiles/urls.py
+++ molo/profiles/urls.py
@@ -2,7 +2,6 @@ from molo.profiles import views
 from molo.profiles.forms import MoloAuthenticationForm
 
 from django.conf.urls import url
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 
@@ -10,32 +9,35 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^logout/$', views.logout_page, name='auth_logout'),
     # If user is not login it will redirect to login page
-    url(r'^login/$', auth_views.login,
-        {'authentication_form': MoloAuthenticationForm}, name='auth_login'),
+    url(r'^login/$',
+        auth_views.LoginView.as_view(
+            authentication_form=MoloAuthenticationForm
+        ),
+        name='auth_login'),
     url(
         r'^register/$',
         views.RegistrationView.as_view(),
         name='user_register'),
     url(
         r'^register/done/',
-        login_required(views.RegistrationDone.as_view(
+        views.RegistrationDone.as_view(
             template_name="profiles/done.html"
-        )),
+        ),
         name='registration_done'
     ),
     url(
         r'^view/myprofile/$',
-        login_required(views.MyProfileView.as_view()),
+        views.MyProfileView.as_view(),
         name='view_my_profile'
     ),
     url(
         r'^edit/myprofile/$',
-        login_required(views.MyProfileEdit.as_view()),
+        views.MyProfileEdit.as_view(),
         name='edit_my_profile'
     ),
     url(
         r'^password-reset/$',
-        login_required(views.ProfilePasswordChangeView.as_view()),
+        views.ProfilePasswordChangeView.as_view(),
         name="profile_password_change"
     ),
     url(
