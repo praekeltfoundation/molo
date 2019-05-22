f27ae1d4429ed49d5edf235d8497ad2ea16ee04e
sewagodimo <edneciamat@gmail.com>
2019-05-22 15:43:47 +0200

    fix patch tests to mock the method where it'used

--- molo/core/tests/test_middleware.py
+++ molo/core/tests/test_middleware.py
@@ -8,9 +8,8 @@ from django.test.client import RequestFactory
 from django.contrib.sessions.middleware import SessionMiddleware
 from django.core.urlresolvers import reverse
 
-import google_analytics  # noqa
 from molo.core.middleware import MoloGoogleAnalyticsMiddleware
-
+from django.contrib.auth.models import User
 from molo.core.models import Main, Languages, SiteLanguageRelation
 from molo.core.tests.base import MoloTestCaseMixin
 from wagtail.search.backends import get_search_backend
@@ -30,21 +29,30 @@ class TestUtils(TestCase, MoloTestCaseMixin):
         # Creates a section under the index page
         self.english_section = self.mk_section(
             self.section_index, title='English section')
+        self.user = User.objects.create_superuser(
+            username='testuser', password='password', email='test@email.com')
 
-    def make_fake_request(self, url, headers={}):
+        profile = self.user.profile
+        profile.alias = 'The Alias'
+        profile.mobile_number = '+27784667723'
+        profile.save()
+
+    def make_fake_request(self, url, headers={}, user=None):
         """
         We don't have any normal views, so we're creating fake
         views using django's RequestFactory
         """
         rf = RequestFactory()
         request = rf.get(url, **headers)
+        if user:
+            request.user = self.user
         session_middleware = SessionMiddleware()
         session_middleware.process_request(request)
         request.session.save()
         return request
 
     @responses.activate
-    @patch("google_analytics.utils.build_ga_params")
+    @patch("molo.core.middleware.build_ga_params")
     def test_ga_middleware(self, mock_method):
         self.backend = get_search_backend('default')
         self.backend.reset_index()
@@ -78,10 +86,42 @@ class TestUtils(TestCase, MoloTestCaseMixin):
         request = self.make_fake_request(
             '/search/?q=Test', headers)
 
+        middleware = MoloGoogleAnalyticsMiddleware()
+        account = ''
+        middleware.submit_tracking(
+            account, request, response, custom_params)
 
-        # middleware.submit_tracking(account,request,response,custom_params,)
+        self.assertTrue('custom_params' in mock_method._mock_call_args[1])
+        self.assertFalse('user_id' in mock_method._mock_call_args[1])
+        self.assertTrue(mock_method._mock_call_args[1]['custom_params'],
+                        custom_params)
+
+    @patch('molo.core.middleware.build_ga_params')
+    def test_ga_submit_tracking_with_custom_params__authenticated(
+            self, mock_method):
+        self.backend = get_search_backend('default')
+        self.backend.reset_index()
+        self.mk_articles(self.english_section, count=2)
+        self.backend.refresh_index()
+
+        custom_params = {'cd2': '1235-245'}
+
+        response = self.client.get(reverse('search'), {
+            'q': 'Test'
+        })
+        headers = {'HTTP_X_IORG_FBS_UIP': '100.100.200.10'}
+        request = self.make_fake_request(
+            '/search/?q=Test', headers, self.user)
+
+        middleware = MoloGoogleAnalyticsMiddleware()
+        account = ''
+        middleware.submit_tracking(
+            account, request, response, custom_params,)
+
         # check if uuid was there
-        print("mock method: ", mock_method.__dict__)
-
-        # mock_method.assert_called()
-        mock_method.assert_called_with(custom_params)
+        for arg in mock_method._mock_call_args:
+            print("here: ", arg)
+        self.assertTrue('user_id' in mock_method._mock_call_args[1])
+        self.assertTrue('custom_params' in mock_method._mock_call_args[1])
+        self.assertTrue(mock_method._mock_call_args[1]['custom_params'],
+                        custom_params)
