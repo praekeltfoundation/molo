c6d79d3cc1ea870d2026f57c7d28b164d5c8e8d8
Aphiwe Moshesh <aphiwemoshesh@gmail.com>
2020-01-16 11:05:37 +0200

    update molo plugins, remove depricated

--- molo/core/known_plugins.py
+++ molo/core/known_plugins.py
@@ -1,8 +1,10 @@
 
 def known_plugins():
-    plugins = [('molo.core', 'Molo'), ('molo.profiles', 'Profiles'),
-               ('molo.commenting', 'Commenting'), ('molo.polls', 'Polls'),
-               ('molo.yourwords', 'YourWords'), ('molo.surveys', 'Surveys'),
-               ('molo.servicedirectory', 'Service Directory')]
+    plugins = [
+        ('molo.core', 'Molo'),
+        ('molo.commenting', 'Commenting'),
+        ('molo.forms', 'Forms'),
+        ('molo.servicedirectory', 'Service Directory')
+    ]
 
     return plugins
