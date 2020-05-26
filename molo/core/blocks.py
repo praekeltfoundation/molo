398f54f4ad8c8007986390a251e6eef106de6de5
Kaitlyn Crawford <kaitlyn@praekelt.com>
2020-05-26 20:43:27 +0200

    Overwrite markdown block api representation to return only markdown

--- molo/core/blocks.py
+++ molo/core/blocks.py
@@ -1,3 +1,5 @@
+import html2markdown
+import re
 from bs4 import BeautifulSoup
 from django.utils.safestring import mark_safe
 from markdown import markdown
@@ -22,6 +24,42 @@ class MarkDownBlock(blocks.TextBlock):
             ],
         )
         return mark_safe(md)
+
+    def get_api_representation(self, value, context=None):
+        """
+        This identifies any content that contains html and converts it into
+        the quivalent markdown for sending via the API
+        """
+        # Remove an invalid html tag that is often present in the content
+        value = value.replace("</br>", "\n")
+
+        has_html = bool(BeautifulSoup(value, "html.parser").find())
+        if not has_html:
+            return self.get_prep_value(value)
+
+        # Some content has markdown and html. We convert it to html first
+        # to remove duplicate formatting and then to markdown
+        html_value = markdown(
+            value,
+            extensions=[
+                'markdown.extensions.fenced_code',
+                'codehilite',
+            ],
+        )
+        # This replaces newlines inside paragraphs with a space
+        html_value = re.sub(r'\n(?!<p>)', ' ', html_value)
+
+        markdown_value = html2markdown.convert(html_value)
+
+        # Remove any duplicated markdown
+        markdown_value = markdown_value.replace("____", "__")
+        markdown_value = markdown_value.replace("****", "**")
+        # Remove underlines since there isn't a markdown equivalent
+        markdown_value = markdown_value.replace("<u>", "")
+        markdown_value = markdown_value.replace("</u>", "")
+        # Remove any escape characters placed in front of markdown
+        markdown_value = markdown_value.replace("\\", "")
+        return markdown_value
 
 
 class MultimediaBlock(AbstractMediaChooserBlock):
