bbeaa4bfb956529081e701cfa81ea413ab13e750
Codie Roelf <codiebeulaine@gmail.com>
2021-03-03 23:03:44 +0200

    add back model admins for api import

---
+++ molo/core/admin.py
@@ -0,0 +1,16 @@
+from molo.core.models import ArticlePage, SectionPage
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin as WagtailModelAdmin, modeladmin_register
+)
+
+class SectionPageAdmin(WagtailModelAdmin):
+    model = SectionPage
+
+
+modeladmin_register(SectionPageAdmin)
+
+class ArticlePageModelAdmin(WagtailModelAdmin):
+    model = ArticlePage
+
+
+modeladmin_register(ArticlePageModelAdmin)
